13a83bdad015008bb2f086cbc7dc47ceb0f57bbd
James-Francis-P <68779462+James-Francis-P@users.noreply.github.com>
2020-07-28 13:59:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var uc = require('upper-case');
+var myVar ="node package manager";
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  /*Use our upper-case module to upper case a string:*/
+  res.write(uc.upperCase(myVar));
+  res.end();
+}).listen(8080);
